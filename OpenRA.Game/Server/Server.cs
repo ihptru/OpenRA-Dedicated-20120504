b2e85291d303d4edb5742b6fa400708d65104813
Igor Popov <ihptru@gmail.com>
2012-05-26 17:19:04 +0400

    fix #5; kill the started game if it's running for too long

--- OpenRA.Game/Server/Server.cs
+++ OpenRA.Game/Server/Server.cs
@@ -16,6 +16,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Timers;
 using OpenRA.FileFormats;
 using OpenRA.GameRules;
 using OpenRA.Network;
@@ -463,6 +464,20 @@ namespace OpenRA.Server
 
 			foreach (var t in ServerTraits.WithInterface<IStartGame>())
 				t.GameStarted(this);
+
+			// Kill started game after 5 hours
+			aTimer = new System.Timers.Timer(18000000);
+			aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+			aTimer.Enabled = true;
 		}
+		
+		private static System.Timers.Timer aTimer;
+		
+		private static void OnTimedEvent(object source, ElapsedEventArgs e)
+		{
+        	Console.WriteLine("Started game has Timed Out at {0} !!!", e.SignalTime);
+			System.Environment.Exit(0);
+		}
+
 	}
 }
